dc3b9288d36edfbf56f9febea332f3ec063ce8f3
Chinaza17 <chinazanwabuike17@gmail.com>
2024-10-22 20:41:51 +0100

    final high_availability commit

---
+++ ec2_security_group.tf
@@ -0,0 +1,39 @@
+# Create a new security group for the instances
+resource "aws_security_group" "naz_sg" {
+  name   = "naz_sg"
+  vpc_id = aws_vpc.myvpc.id
+  description = "allow all traffic"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "naz-tesgSG"  
+  }
+}
